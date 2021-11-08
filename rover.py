819120ada91d6a745e629a8e6d3631e76c336350
Feraklin <55380723+Feraklin@users.noreply.github.com>
2021-11-08 11:45:45 +0300

    Add files via upload

---
+++ rover.py
@@ -0,0 +1,68 @@
+import json
+import time
+
+start_time = time.time()
+
+
+def rover(map):
+    mapWidht = len(map[0])  # количество столбцов "карты"
+    S = len(map) * mapWidht  # Количество элементов "карты"
+    bestWay = []  # Кратчайший путь
+    shortWay = [100000] * S  # Матрица для хранения коротких путей
+    shortWay[0] = 0
+    calcWay = [100000] * S  # Матрица для расчетов коротких путей
+    calcWay[0] = 0
+    parents = [-1] * S  # Матрица для хранения точек пути
+    distances = [0] * S  # Матрица для расчета расстояния между соседними точками
+    for i in range(S):
+        distances[i] = [0] * S
+
+    # Заполняем матрицу расстояний между соседними точками
+    for i in range(S):
+        for j in range(i + 1, S):
+            if (
+                i // mapWidht == j // mapWidht and abs(i % mapWidht - j % mapWidht) == 1
+            ) or (
+                abs(i // mapWidht - j // mapWidht) == 1 and i % mapWidht == j % mapWidht
+            ):
+                distances[i][j] = distances[j][i] = (
+                    abs(
+                        map[i // mapWidht][i % mapWidht]
+                        - map[j // mapWidht][j % mapWidht]
+                    )
+                    + 1
+                )
+
+    # Алгоритм Дейкстры:
+    # Считаем, пока минимальное значение в матрице для расчета меньше текущего лучшего
+    while min(calcWay) < shortWay[S - 1]:
+        i = calcWay.index(min(calcWay))
+        # Сразу меняем значение на очень большое, нам нужен был только индекс
+        calcWay[i] = 2000000
+        for j in range(S):
+            # Если сосед и если результат "шага" не превысит текущее лучшее значение
+            if distances[i][j] > 0 and shortWay[j] > shortWay[i] + distances[i][j]:
+                # Записываем результаты шага и точку, из которой пришли
+                shortWay[j] = calcWay[j] = shortWay[i] + distances[i][j]
+                parents[j] = i
+
+    # Воспроизводим путь от финальной тучки в первоначальную и "разворачиваем" его
+    i = S - 1
+    bestWay.append([i // mapWidht, i % mapWidht])
+    while i > 0:
+        bestWay.append([parents[i] // mapWidht, parents[i] % mapWidht])
+        i = parents[i]
+
+    bestWay = bestWay[::-1]
+    return (len(bestWay) - 1, shortWay[S - 1], bestWay)
+
+
+with open(r"d:\Programming\1\PodgyzBot\rover\map.json", "r", encoding="utf-8") as f:
+    map = json.load(f)["map"]
+
+results = rover(map)
+
+print(results[2])
+print("steps: " + str(results[0]))
+print("fuel: " + str(results[1]))
+print("--- %s seconds ---" % (time.time() - start_time))

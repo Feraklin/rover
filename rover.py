7785c991ec2d0f9770080a7ab5ef69b5aaf7bdf9
Feraklin <feraklin@ya.ru>
2021-11-10 10:40:10 +0300

    refactoring some var for pep8

--- rover.py
+++ rover.py
@@ -5,44 +5,44 @@ start_time = time.time()
 
 
 def rover(map):
-    mapWidht = len(map[0])  # количество столбцов "карты"
-    S = len(map) * mapWidht  # Количество точек "карты"
+    map_widht = len(map[0])
+    all_points = len(map) * map_widht
     bestWay = []  # Кратчайший путь
-    shortWay = [100000] * S  # Матрица для хранения коротких путей
+    shortWay = [100000] * all_points  # Матрица для хранения коротких путей
     shortWay[0] = 0
-    calcWay = [100000] * S  # Матрица для расчетов коротких путей
+    calcWay = [100000] * all_points  # Матрица для расчетов коротких путей
     calcWay[0] = 0
-    parents = [-1] * S  # Матрица для хранения точек пути
+    parents = [-1] * all_points  # Матрица для хранения точек пути
     # Матрица для расчета расстояния между соседними точками
-    distances = [0] * S
-    for i in range(S):
-        distances[i] = [0] * S
+    distances = [0] * all_points
+    for i in range(all_points):
+        distances[i] = [0] * all_points
 
     # Заполняем матрицу расстояний между соседними точками
-    for i in range(S):
-        for j in range(i + 1, S):
+    for i in range(all_points):
+        for j in range(i + 1, all_points):
             if (
-                i // mapWidht == j // mapWidht and abs(i %
-                                                       mapWidht - j % mapWidht) == 1
+                i // map_widht == j // map_widht and abs(i %
+                                                         map_widht - j % map_widht) == 1
             ) or (
-                abs(i // mapWidht - j //
-                    mapWidht) == 1 and i % mapWidht == j % mapWidht
+                abs(i // map_widht - j //
+                    map_widht) == 1 and i % map_widht == j % map_widht
             ):
                 distances[i][j] = distances[j][i] = (
                     abs(
-                        map[i // mapWidht][i % mapWidht]
-                        - map[j // mapWidht][j % mapWidht]
+                        map[i // map_widht][i % map_widht]
+                        - map[j // map_widht][j % map_widht]
                     )
                     + 1
                 )
 
     # Алгоритм Дейкстры:
     # Считаем, пока минимальное значение в матрице для расчета меньше текущего лучшего на финише
-    while min(calcWay) < shortWay[S - 1]:
+    while min(calcWay) < shortWay[all_points - 1]:
         i = calcWay.index(min(calcWay))
         # Сразу меняем значение на очень большое, нам нужен был только индекс
         calcWay[i] = 2000000
-        for j in range(S):
+        for j in range(all_points):
             # Если сосед и если результат "шага" не превысит текущее лучшее значение
             if distances[i][j] > 0 and shortWay[j] > shortWay[i] + distances[i][j]:
                 # Записываем результаты шага и точку, из которой пришли
@@ -50,14 +50,14 @@ def rover(map):
                 parents[j] = i
 
     # Воспроизводим путь от финальной точки в первоначальную и "разворачиваем" его
-    i = S - 1
-    bestWay.append([i // mapWidht, i % mapWidht])
+    i = all_points - 1
+    bestWay.append([i // map_widht, i % map_widht])
     while i > 0:
-        bestWay.append([parents[i] // mapWidht, parents[i] % mapWidht])
+        bestWay.append([parents[i] // map_widht, parents[i] % map_widht])
         i = parents[i]
 
     bestWay = bestWay[::-1]
-    return (len(bestWay) - 1, shortWay[S - 1], bestWay)
+    return (len(bestWay) - 1, shortWay[all_points - 1], bestWay)
 
 
 with open(r"D:\Программирование\1\PodgyzBot\rover\map.json", "r", encoding="utf-8") as f:
